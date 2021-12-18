da9c460c87a1a618a72deb8e7dfb7391fe4e7f2a
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-12-19 00:28:15 +0200

    checking if frontend values are received

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { pool } = require('./dbConfig');
 
 require('dotenv').config();
 
@@ -6,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -20,7 +22,20 @@ app.get('/users/login', (req, res) => {
 });
 
 app.get('/users/dashboard', (req, res) => {
-  res.render('dashboard', { user: 'Douglas'});
+  res.render('dashboard', { user: 'Douglas' });
+});
+
+app.post('/users/register', (req, res) => {
+  let { name, email, password, password2 } = req.body;
+
+  console.log({
+    name,
+    email,
+    password,
+    password2
+  });
+
+  let errors = [];
 });
 
 app.listen(PORT, (req, res) => {
