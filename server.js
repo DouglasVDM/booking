671ffed5352fa998f0f117898587c396b6b8eb40
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-12-18 21:19:49 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+
+
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is listening on port: ${PORT}. Ready to accept requests!`);
+});
