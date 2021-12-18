895b992ddf5c194e36f04c62d29837b0d116f29a
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-12-18 21:35:28 +0200

    render html for basic routes

--- server.js
+++ server.js
@@ -5,9 +5,23 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
+app.get('/users/register', (req, res) => {
+  res.render('register');
+});
 
+app.get('/users/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/users/dashboard', (req, res) => {
+  res.render('dashboard', { user: 'Douglas'});
+});
 
 app.listen(PORT, (req, res) => {
   console.log(`Server is listening on port: ${PORT}. Ready to accept requests!`);
