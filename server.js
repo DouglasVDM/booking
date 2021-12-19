65a1788eecfbb82902c3928bce5bea9ab84d1465
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-12-19 22:55:32 +0200

    post new user to database

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { pool } = require('./dbConfig');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const flash = require('express-flash');
 
 require('dotenv').config();
 
@@ -8,6 +11,14 @@ const PORT = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+  })
+);
+app.use(flash());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -25,7 +36,7 @@ app.get('/users/dashboard', (req, res) => {
   res.render('dashboard', { user: 'Douglas' });
 });
 
-app.post('/users/register', (req, res) => {
+app.post('/users/register', async (req, res) => {
   let { name, email, password, password2 } = req.body;
 
   console.log({
@@ -38,19 +49,50 @@ app.post('/users/register', (req, res) => {
   let errors = [];
 
   if (!name || !email || !password || !password2) {
-    errors.push({message:'Please enter all fields'});
+    errors.push({ message: 'Please enter all fields' });
   }
 
   if (password.length < 6) {
-    errors.push({message:'Password should be at least 6 characters'});
+    errors.push({ message: 'Password should be at least 6 characters' });
   }
 
   if (password !== password2) {
-    errors.push({message:'Passwords do not match'});
+    errors.push({ message: 'Passwords do not match' });
   }
 
-  if(errors.length > 0) {
+  if (errors.length > 0) {
     res.render('register', { errors });
+  } else {
+    // Form validation has passed
+
+    let hashedPassword = await bcrypt.hash(password, 10);
+    console.log('hashedPassword:', hashedPassword);
+
+    pool.query(
+      `SELECT * FROM users WHERE email = $1`, [email], (err, results) => {
+        if (err) {
+          throw err;
+        }
+        console.log('results.rows:', results.rows);
+
+        if (results.rows.length > 0) {
+          errors.push({ message: 'Email already registered' });
+          res.render('register', { errors })
+        } else {
+          pool.query(
+            `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, password`, [name, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              console.log('results.rows:', results.rows);
+              req.flash('success_msg', 'You are now registered. Please log in');
+              res.redirect('/users/login');
+            }
+          )
+        }
+      }
+    )
   }
 });
 
